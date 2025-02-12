a7afbec725b0e1b64d8a8ea3e707ddd83dd72bcf
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2025-02-12 21:04:55 +0100

    Hemos creado la función para cambiar el idioma

---
+++ js/sociosColaboradores.js
@@ -0,0 +1,24 @@
+//Creamos una función para cambiar de idioma
+function changeLanguage(language) {
+    //Guardar el idioma en el LocalStorage
+    localStorage.setItem('language', language);
+
+    //Actualizar el idioma de los elementos
+    const elements = document.querySelectorAll('[data-en], [data-es]');
+    elements.forEach(element => {
+        if (language === 'en') {
+            element.textContent = element.getAttribute('data-en');
+        } else if (language === 'es') {
+            element.textContent = element.getAttribute('data-es');
+        }
+    });
+
+    //Cambiamos el atributo lang del documento
+    document.documentElement.lang = language;
+}
+
+//Cargar el idioma guardado cuando la página se cargue
+window.onload = function () {
+    const savedLanguage = localStorage.getItem('language') || 'es'; //Valor por defecto: español
+    changeLanguage(savedLanguage);
+}
