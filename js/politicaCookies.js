cdc352cbe8bd584496aa5b70448eef8f4310e353
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2025-03-23 18:55:25 +0100

    Hemos modificado los archivos de css, html y js de la política de Privacidad, Cookies y Aviso Legal

--- js/politicaCookies.js
+++ js/politicaCookies.js
@@ -1,9 +1,9 @@
-//Creamos una función para cambiar de idioma
+// Función para cambiar el idioma
 function changeLanguage(language) {
-    //Guardar el idioma en el LocalStorage
+    // Guardar el idioma en el LocalStorage
     localStorage.setItem('language', language);
 
-    //Actualizar el idioma de los elementos
+    // Actualizar el idioma de los elementos
     const elements = document.querySelectorAll('[data-en], [data-es]');
     elements.forEach(element => {
         if (language === 'en') {
@@ -13,6 +13,22 @@ function changeLanguage(language) {
         }
     });
 
-    //Cambiamos el atributo lang del documento
+    // Cambiar el atributo lang del documento
     document.documentElement.lang = language;
+
+    // Cambiar el texto del botón
+    document.getElementById("change-lang").textContent = language === 'en' ? 'ES' : 'ENG';
 }
+
+// Cargar el idioma desde el LocalStorage y actualizar la página al cargar
+window.addEventListener('load', () => {
+    const savedLanguage = localStorage.getItem('language') || 'es'; // Si no hay idioma guardado, usamos 'es' como predeterminado
+    changeLanguage(savedLanguage);
+});
+
+// Cambiar el idioma al hacer clic en el botón
+document.getElementById("change-lang").addEventListener('click', () => {
+    const currentLanguage = document.documentElement.lang;
+    const newLanguage = currentLanguage === 'en' ? 'es' : 'en';
+    changeLanguage(newLanguage);
+});
